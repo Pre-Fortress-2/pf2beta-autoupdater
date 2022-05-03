40743119bc9bb17bced2af33ef082f1814a70608
dangreene0 <35577693+dangreene0@users.noreply.github.com>
2022-05-03 17:34:20 -0400

    Added remote file checking and comparing
    - Added new logging

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ import sys
 import os
 import time
 import platform
+import logging
+
+REM_BRANCH = 'master'
+logging.basicConfig(handlers=[logging.FileHandler(filename="updater.log", 
+                                                  encoding='utf-8', mode='a+')],
+                    format="[ %(asctime)s ] %(message)s",
+                    datefmt="%m/%d/%Y %I:%M:%S %p",
+                    level=logging.DEBUG)
+console = logging.StreamHandler()
+console.setLevel(logging.WARNING)
+console.setFormatter(logging.Formatter("[ %(asctime)s ] %(message)s", "%m/%d/%Y %I:%M:%S %p"))
+logging.getLogger('').addHandler(console)
 
 def pullRepo(localRepo):
     o = localRepo.remotes.origin
     try:
-        print("Attempting to pull")
+        logging.warning("Attempting to pull")
         o.pull()
-        print("Success")
+        logging.warning("Success")
     except Exception as e:
-        print("Unable to pull repo, please make this user has the proper credentials.")
+        logging.warning("Unable to pull repo, please make this user has the proper credentials.")
         print(e)
     
 def restartServer(server, port, password, screen, localRepo):
@@ -44,10 +56,10 @@ def restartServer(server, port, password, screen, localRepo):
             pullRepo(localRepo)
             os.system(f"screen -dmS {screen} ./run.sh")
         except Exception as e:
-            print("Unable restart server, check users perms")
+            logging.warning("Unable restart server, check users perms")
             print(e)
     if platform.system().lower() == "windows":
-        print("Please relaunch your server.")
+        logging.warning("Please relaunch your server.")
         
 def parseCommit(config):
 
@@ -60,17 +72,40 @@ def parseCommit(config):
     localRepo = git.Repo("../pf2beta")
     main = localRepo.head.reference
     gitmsg = main.commit.message
-    
-    print("Checking out " + str(main))
-    
-    print(str(gitmsg))    
-     
-    if key.lower() in gitmsg.lower():
-        print("Critical server update detected")
-        restartServer(server, port, password, screen, localRepo)
-    else:
-        print(f"Keyword \'{key}\' not found\n")
-        pullRepo(localRepo)
+   
+    logging.warning("Checking out " + str(main))
+    logging.warning(str(gitmsg))    
+
+    def pretty_diff(diff):
+        for cht in diff.change_type:
+            changes = list(diff.iter_change_type(cht))
+            if len(changes) == 0:
+                continue
+            logging.warning("Changes type:", cht)
+            for d in changes:
+                print(d.b_path)
+                if key in d.b_path:
+                    return True
+
+    while True:
+        current_hash = localRepo.head.object.hexsha
+        o = localRepo.remotes.origin
+        o.fetch()
+        changed = o.refs[REM_BRANCH].object.hexsha != current_hash
+        if changed:
+            diff = localRepo.head.commit.diff(o.refs[REM_BRANCH].object.hexsha)
+            restart = pretty_diff(diff)
+            if restart:
+                logging.warning("Critical server update detected")
+                restartServer(server, port, password, screen, localRepo)
+                break
+            else:
+                logging.warning(f"Keyword \'{key}\' not found\n")
+                pullRepo(localRepo)
+                break
+        else:
+            logging.warning("No update detected")
+            break
 
 def main():
     try:
@@ -78,7 +113,7 @@ def main():
             config = json.load(json_file)
 
     except Exception as e:
-        print("Unable to open config.json - Please check it is in the root directory")
+        logging.warning("Unable to open config.json - Please check it is in the root directory")
         print(e)
         sys.exit(1)
         
